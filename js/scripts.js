a2d300725cee05ccbcfdc803997ef07225ff1a5c
pazgalev <pazgalev@tutu.tech>
2023-03-18 19:28:05 +0300

    use grid

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-function myResponsiveFunc(){
-    var nav=document.getElementById("main-nav-list");
-    if(nav.className==="main-nav-list"){
-        nav.className+=" responsive";
-    }else{
-        nav.className="main-nav-list";
-    }
-
-}
